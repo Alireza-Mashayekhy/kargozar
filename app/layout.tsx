0e6dd57d223fb0fa06b077e18e241490d5f3881b
Alireza_Mashayekhy <alirezamashayekhy2000@gmail.com>
2024-07-09 14:55:09 +0330

    config

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,8 @@ import Script from 'next/script';
 
 export default function RootLayout({
     children,
-    team,
 }: {
     children: React.ReactNode;
-    team: React.ReactNode;
 }) {
     const jsonLd: WithContext<Person> = {
         '@context': 'https://schema.org',
