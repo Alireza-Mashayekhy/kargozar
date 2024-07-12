e5a14af1224e9dad3c833e160059318168768236
alireza <alirezamashayekhy2000@gmail.com>
2024-07-12 12:26:51 +0330

    fix bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,135 +1,126 @@
-'use client';
-import Banner from '@/components/Banner';
-import BannerSection from '@/components/BannerSection';
-import BlogSection from '@/components/Blog/Section';
+"use client";
+import Banner from "@/components/Banner";
+import BannerSection from "@/components/BannerSection";
+import BlogSection from "@/components/Blog/Section";
 
-import axios from 'axios';
+import axios from "axios";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-    const [blogs, setBlogs] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const banners = ['/landing/banner_1.webp', '/landing/banner_2.webp'];
+  const banners = ["/landing/banner_1.webp", "/landing/banner_2.webp"];
 
-    const mobileBanners = [
-        '/landing/mobileBanner_1.webp',
-        '/landing/mobileBanner_2.webp',
-    ];
+  const mobileBanners = [
+    "/landing/mobileBanner_1.webp",
+    "/landing/mobileBanner_2.webp",
+  ];
 
-    interface servicesCategoriesType {
-        id: number;
-        image: string;
-        title: string;
-        link: string;
-    }
-    useEffect(() => {
-        axios.get(`https://api.kargozargomrok.com/api/feed`).then((res) => {
-            const myData: never[] = [];
-            res.data.data.circular_letters.map((el: never) => {
-                myData.push(el);
-            });
-            res.data.data.news_letters.map((el: never) => {
-                myData.push(el);
-            });
-            res.data.data.posts.map((el: never) => {
-                myData.push(el);
-            });
-            setBlogs(myData);
-            setLoading(false);
-        });
-    }, []);
+  interface servicesCategoriesType {
+    id: number;
+    image: string;
+    title: string;
+    link: string;
+  }
+  useEffect(() => {
+    axios.get(`https://api.kargozargomrok.com/api/feed`).then((res) => {
+      const myData: never[] = [];
+      res.data.data.circular_letters.map((el: never) => {
+        myData.push(el);
+      });
+      res.data.data.news_letters.map((el: never) => {
+        myData.push(el);
+      });
+      res.data.data.posts.map((el: never) => {
+        myData.push(el);
+      });
+      setBlogs(myData);
+      setLoading(false);
+    });
+  }, []);
 
-    const servicesCategories: servicesCategoriesType[] = [
-        {
-            id: 1,
-            image: '/landing/tarkhisService.png',
-            title: 'ترخیص کالا',
-            link: '/service/clearance',
-        },
-        {
-            id: 2,
-            image: '/landing/komesionService.png',
-            title: 'کمیسیون اختلافات گمرک',
-            link: '/service/commission',
-        },
-        {
-            id: 3,
-            image: '/landing/moshavereService.png',
-            title: 'مشاوره بازرگانی',
-            link: '/service/counseling',
-        },
-    ];
+  const servicesCategories: servicesCategoriesType[] = [
+    {
+      id: 1,
+      image: "/landing/tarkhisService.png",
+      title: "ترخیص کالا",
+      link: "/service/clearance",
+    },
+    {
+      id: 2,
+      image: "/landing/komesionService.png",
+      title: "مراجع اختالفات گمرک",
+      link: "/service/commission",
+    },
+    {
+      id: 3,
+      image: "/landing/moshavereService.png",
+      title: "مشاوره بازرگانی",
+      link: "/service/counseling",
+    },
+  ];
 
-    return (
-        <div>
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1"
-            />
-            <meta charSet="utf-8" />
-            <title>کارگزار رسمی گمرک - خانه</title>
-            <meta
-                name="description"
-                content="مجموعه کارگزار گمرک با مدیریت کارگزار رسمی گمرک ایران مهندس
+  return (
+    <div>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <title>کارگزار رسمی گمرک - خانه</title>
+      <meta
+        name="description"
+        content="مجموعه کارگزار گمرک با مدیریت کارگزار رسمی گمرک ایران مهندس
                     محمدامین قنبری تشکیل شده است تا نیاز های شما بازرگان وصاحب
                     کالای عزیز را بر طرف سازد.  "
-            />
-            <meta property="og:title" content="کارگزار رسمی گمرک" />
-            <meta property="og:type" content="image" />
-            <meta property="og:url" content="/logo/KGLogo.png" />
-            <meta
-                property="og:image"
-                content="مجموعه کارگزار گمرک با مدیریت کارگزار رسمی گمرک ایران مهندس
+      />
+      <meta property="og:title" content="کارگزار رسمی گمرک" />
+      <meta property="og:type" content="image" />
+      <meta property="og:url" content="/logo/KGLogo.png" />
+      <meta
+        property="og:image"
+        content="مجموعه کارگزار گمرک با مدیریت کارگزار رسمی گمرک ایران مهندس
                     محمدامین قنبری تشکیل شده است تا نیاز های شما بازرگان وصاحب
                     کالای عزیز را بر طرف سازد."
-            />
-            <meta property="og:description" content="/logo/KGLogo.png" />
-            <link rel="canonical" href="https://kargozargomrok.com" />
-            <div className="flex flex-col gap-10 md:gap-16">
-                <div className="flex flex-col gap-16 px-2.5 md:px-10 pt-28 md:pt-44">
-                    <h1 className="hidden">
-                        <b>
-                            مجموعه کارگزار گمرک با مدیریت مهندس محمدامین قنبری
-                        </b>
-                    </h1>
-                    <div>
-                        <Banner data={banners} mobileData={mobileBanners} />
-                        <div className="w-full sm:w-11/12 md:w-4/5 mt-5 sm:mt-0 bg-white py-3 sm:py-6 rounded-xl sm:rounded-2xl shadow-md mx-auto sm:-top-5 md:-top-12 relative z-10 flex items-center justify-around">
-                            {servicesCategories.map((item) => {
-                                return (
-                                    <Link
-                                        href={item.link}
-                                        key={item.id}
-                                        className="flex flex-col items-center gap-1.5 cursor-pointer"
-                                    >
-                                        <Image
-                                            alt={`landing-${item.title}`}
-                                            title={item.title}
-                                            src={item.image}
-                                            className="w-10 sm:w-16"
-                                            width={500}
-                                            height={500}
-                                        />
-                                        <div className="text-sm sm:text-xl text-center">
-                                            {item.title}
-                                        </div>
-                                    </Link>
-                                );
-                            })}
-                        </div>
+      />
+      <meta property="og:description" content="/logo/KGLogo.png" />
+      <link rel="canonical" href="https://kargozargomrok.com" />
+      <div className="flex flex-col gap-10 md:gap-16">
+        <div className="flex flex-col gap-16 px-2.5 md:px-10 pt-28 md:pt-44">
+          <h1 className="hidden">
+            <b>مجموعه کارگزار گمرک با مدیریت مهندس محمدامین قنبری</b>
+          </h1>
+          <div>
+            <Banner data={banners} mobileData={mobileBanners} />
+            <div className="w-full sm:w-11/12 md:w-4/5 mt-5 sm:mt-0 bg-white py-3 sm:py-6 rounded-xl sm:rounded-2xl shadow-md mx-auto sm:-top-5 md:-top-12 relative z-10 flex items-center justify-around">
+              {servicesCategories.map((item) => {
+                return (
+                  <Link
+                    href={item.link}
+                    key={item.id}
+                    className="flex flex-col items-center gap-1.5 cursor-pointer"
+                  >
+                    <Image
+                      alt={`landing-${item.title}`}
+                      title={item.title}
+                      src={item.image}
+                      className="w-10 sm:w-16"
+                      width={500}
+                      height={500}
+                    />
+                    <div className="text-sm sm:text-xl text-center">
+                      {item.title}
                     </div>
-                    <BannerSection />
-                </div>
-                <BlogSection
-                    title="مجله گمرکی"
-                    data={blogs}
-                    loading={loading}
-                />
+                  </Link>
+                );
+              })}
             </div>
+          </div>
+          <BannerSection />
         </div>
-    );
+        <BlogSection title="مجله گمرکی" data={blogs} loading={loading} />
+      </div>
+    </div>
+  );
 }
--- components/Blog/Section.tsx
+++ components/Blog/Section.tsx
@@ -1,113 +1,134 @@
-'use client';
-import Link from 'next/link';
-import { useEffect, useRef, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import BlogCard from './Card';
+"use client";
+import Link from "next/link";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import BlogCard from "./Card";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
 export default function BlogSection(props: any) {
-    const sliderRef = useRef<any>(null);
+  const sliderRef = useRef<any>(null);
 
-    const [sliderIndex, setSliderIndex] = useState(0);
-    const [slidePerView, setSlidePerView] = useState(4);
+  const [sliderIndex, setSliderIndex] = useState(0);
+  const [slidePerView, setSlidePerView] = useState(4);
 
-    const checkWidth = () => {
-        if (window.innerWidth > 1280) {
-            setSlidePerView(4);
-        } else if (window.innerWidth > 1024) {
-            setSlidePerView(3);
-        } else if (window.innerWidth > 768) {
-            setSlidePerView(2);
-        } else {
-            setSlidePerView(1);
-        }
-    };
-
-    useEffect(() => {
-        checkWidth();
-        window.addEventListener('resize', checkWidth);
-    }, []);
-
-    interface blogType {
-        title: string;
-        images: Array<string>;
-        slug: string;
-        upper_content: string;
-        type: string;
+  const handlePrev = useCallback(() => {
+    if (sliderRef.current) {
+      sliderRef.current.swiper.slidePrev();
     }
+  }, []);
 
-    return (
-        <div className="flex flex-col gap-4 sm:gap-8 overflow-hidden">
-            <div className="flex items-center justify-between px-2.5 sm:px-10">
-                <h3 className="text-primary-1 font-bold text-xl sm:text-3xl">
-                    {props?.title}
-                </h3>
-                <Link
-                    href="/blog"
-                    className="bg-primary-1 px-4 py-1.5 sm:px-8 sm:py-3 text-white rounded-md sm:rounded-xl font-bold text-xs sm:text-base"
-                >
-                    ورود به مجله
-                </Link>
-            </div>
-            <div className="bg-primary-2 pt-7 sm:pt-16 pb-28">
-                <Swiper
-                    onSlideChange={(e: any) => setSliderIndex(e.activeIndex)}
-                    ref={sliderRef}
-                    className="w-full relative !px-2.5 sm:!px-10 !overflow-visible"
-                    spaceBetween={48}
-                    slidesPerView={slidePerView}
-                >
-                    {props.loading &&
-                        [...new Array(15)].map(
-                            (item: blogType, index: Number) => {
-                                return (
-                                    <SwiperSlide
-                                        key={`blog-${index}`}
-                                        className=" !h-auto"
-                                    >
-                                        <BlogCard isSkeleton />
-                                    </SwiperSlide>
-                                );
-                            }
-                        )}
-                    {!props.loading &&
-                        props.data?.map((item: blogType, index: Number) => {
-                            return (
-                                <SwiperSlide
-                                    key={`blog-${index}`}
-                                    className=" !h-auto"
-                                >
-                                    <BlogCard
-                                        title={item?.title}
-                                        image={item?.images[0]}
-                                        slug={`/blog/${item?.type}s/${item?.slug}`}
-                                        description={item?.upper_content}
-                                    />
-                                </SwiperSlide>
-                            );
-                        })}
-                    <div className="flex items-center absolute left-1/2 -translate-x-1/2 gap-2 mt-10">
-                        {props.data?.map((item: any, index: number) => {
-                            if (index < props.data.length - slidePerView + 1) {
-                                return (
-                                    <span
-                                        key={`dot-${index}`}
-                                        className={` h-2.5 rounded-full transition-all cursor-pointer ${
-                                            sliderIndex === index
-                                                ? 'bg-primary-1 w-5'
-                                                : 'bg-secondary w-2.5'
-                                        }`}
-                                        onClick={() => {
-                                            sliderRef.current.swiper.slideTo(
-                                                index
-                                            );
-                                        }}
-                                    />
-                                );
-                            }
-                        })}
-                    </div>
-                </Swiper>
-            </div>
-        </div>
-    );
+  const handleNext = useCallback(() => {
+    if (sliderRef.current) {
+      sliderRef.current.swiper.slideNext();
+    }
+  }, []);
+
+  const checkWidth = () => {
+    if (window.innerWidth > 1280) {
+      setSlidePerView(4);
+    } else if (window.innerWidth > 1024) {
+      setSlidePerView(3);
+    } else if (window.innerWidth > 768) {
+      setSlidePerView(2);
+    } else {
+      setSlidePerView(1);
+    }
+  };
+
+  useEffect(() => {
+    checkWidth();
+    window.addEventListener("resize", checkWidth);
+  }, []);
+
+  interface blogType {
+    title: string;
+    images: Array<string>;
+    slug: string;
+    upper_content: string;
+    type: string;
+  }
+
+  return (
+    <div className="flex flex-col gap-4 sm:gap-8 overflow-hidden">
+      <div className="flex items-center justify-between px-2.5 sm:px-10">
+        <h3 className="text-primary-1 font-bold text-xl sm:text-3xl">
+          {props?.title}
+        </h3>
+        <Link
+          href="/blog"
+          className="bg-primary-1 px-4 py-1.5 sm:px-8 sm:py-3 text-white rounded-md sm:rounded-xl font-bold text-xs sm:text-base"
+        >
+          ورود به مجله
+        </Link>
+      </div>
+      <div className="bg-primary-2 pt-7 sm:pt-16 pb-28">
+        <Swiper
+          onSlideChange={(e: any) => setSliderIndex(e.activeIndex)}
+          ref={sliderRef}
+          className="w-full relative !px-2.5 sm:!px-10 !overflow-visible"
+          spaceBetween={48}
+          slidesPerView={slidePerView}
+        >
+          {props.loading &&
+            [...new Array(15)].map((item: blogType, index: Number) => {
+              return (
+                <SwiperSlide key={`blog-${index}`} className=" !h-auto">
+                  <BlogCard isSkeleton />
+                </SwiperSlide>
+              );
+            })}
+          {!props.loading &&
+            props.data?.map((item: blogType, index: Number) => {
+              return (
+                <SwiperSlide key={`blog-${index}`} className=" !h-auto">
+                  <BlogCard
+                    title={item?.title}
+                    image={item?.images[0]}
+                    slug={`/blog/${item?.type}s/${item?.slug}`}
+                    description={item?.upper_content}
+                  />
+                </SwiperSlide>
+              );
+            })}
+          <div className="flex items-center absolute left-1/2 -translate-x-1/2 gap-2 mt-10">
+            <button
+              onClick={handlePrev}
+              className={` z-10 flex items-center mx-10 justify-center rounded-md bg-primary-1 w-6 sm:w-11 h-6 sm:h-11 ${
+                sliderIndex === 0 ? "opacity-50 cursor-default" : ""
+              }`}
+            >
+              <FaChevronRight className="w-4 sm:w-6 h-4 sm:h-6" color="white" />
+            </button>
+            {props.data?.map((item: any, index: number) => {
+              if (index < props.data.length - slidePerView + 1) {
+                return (
+                  <span
+                    key={`dot-${index}`}
+                    className={` h-2.5 rounded-full transition-all cursor-pointer ${
+                      sliderIndex === index
+                        ? "bg-primary-1 w-5"
+                        : "bg-secondary w-2.5"
+                    }`}
+                    onClick={() => {
+                      sliderRef.current.swiper.slideTo(index);
+                    }}
+                  />
+                );
+              }
+            })}
+            <button
+              onClick={handleNext}
+              className={` z-10 flex items-center mx-10 justify-center rounded-md bg-primary-1 w-6 sm:w-11 h-6 sm:h-11 ${
+                sliderIndex === props.data.length - slidePerView
+                  ? "opacity-50 cursor-default"
+                  : ""
+              }`}
+            >
+              <FaChevronLeft className="w-4 sm:w-6 h-4 sm:h-6" color="white" />
+            </button>
+          </div>
+        </Swiper>
+      </div>
+    </div>
+  );
 }
